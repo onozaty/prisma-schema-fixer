f81636e020aaed215eb0aa5f08dd4aa023c50f54
onozaty <onozaty@gmail.com>
2025-03-08 22:57:26 +0900

    Remove BlockType as it is no longer needed

--- src/blocks/block.ts
+++ src/blocks/block.ts
@@ -1,7 +1,4 @@
 // https://www.prisma.io/docs/orm/reference/prisma-schema-reference
-export const BlockType = ["model", "enum", "other", "none"] as const;
-
-export type BlockType = (typeof BlockType)[number];
 
 export interface Block {
   appendLine(line: string): void;

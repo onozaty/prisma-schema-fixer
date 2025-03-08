7a7fa9fcb94056a1a0ca6f8749d663bf85f41c14
onozaty <onozaty@gmail.com>
2025-03-08 22:56:36 +0900

    Remove src2 folder

--- src2/schema.ts
+++
@@ -1,8 +0,0 @@
-import prismaInternal from "@prisma/internals";
-
-export const formatSchema = async (contents: string) => {
-  const formated = await prismaInternal.formatSchema({
-    schemas: [["dummy.prisma", contents]],
-  });
-  return formated[0][1];
-};
